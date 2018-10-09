22fb8d187872292464f6d851fcedd9f8b9f1aae0
Simon Kowallik <github@simonkowallik.com>
2018-10-09 23:24:36 +0200

    tmsh2iapp.rb: added test + sha256

--- tmsh2iapp.rb
+++ tmsh2iapp.rb
@@ -3,6 +3,7 @@ class Tmsh2iapp < Formula
   homepage "https://github.com/f5devcentral/f5-tmsh2iapp"
   url "https://raw.githubusercontent.com/f5devcentral/f5-tmsh2iapp/00f44aa223b2aa3f9d394c0ef9171887dd24b22c/tmsh2iapp.pl"
   version "20180717.1"
+  sha256 "a8285de5f2f5055f42113a6d255af1797b81e8039737a2ee9164c047c3e9691f"
   head "https://github.com/f5devcentral/f5-tmsh2iapp.git"
 
   #depends_on "JSON" => :perl
@@ -12,5 +13,9 @@ class Tmsh2iapp < Formula
 
   def install
     bin.install "tmsh2iapp.pl" => "tmsh2iapp"
+  end
+
+  test do
+    system "tmsh2iapp", "version"
   end
 end
