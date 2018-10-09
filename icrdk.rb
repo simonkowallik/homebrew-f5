7c18203f41013fb2d4391fe0d18c8c8b3e457450
Simon Kowallik <github@simonkowallik.com>
2018-10-10 00:47:30 +0200

    updated icrdk.rb and tmsh2iapp.rb

--- icrdk.rb
+++ icrdk.rb
@@ -1,15 +1,17 @@
 class Icrdk < Formula
   desc "F5 iControl LX Development Kit"
   homepage "https://github.com/f5devcentral/f5-icontrollx-dev-kit"
-  url "https://github.com/f5devcentral/f5-icontrollx-dev-kit/tree/b2e2a154462a966e244fee12ce0ca8ed86dcc5b3"
+  url "https://github.com/f5devcentral/f5-icontrollx-dev-kit.git", :revision => "b2e2a154462a966e244fee12ce0ca8ed86dcc5b3"
   version "1.0.1"
-  #sha256 ""
   head "https://github.com/f5devcentral/f5-icontrollx-dev-kit.git"
 
   def install
     bin.install "bin/icrdk.js" => "icrdk"
-    bin.install Dir["lib/*"]
-    bin.install Dir["res/*"]
+    lib.install Dir["lib/*"]
+    (prefix/"res").install Dir["res/*"]
+    prefix.install "index.js"
   end
-
+  test do
+    system "icrdk >/dev/null 2>/dev/null"
+  end
 end
--- tmsh2iapp.rb
+++ tmsh2iapp.rb
@@ -1,9 +1,8 @@
 class Tmsh2iapp < Formula
   desc "tmsh2iapp is an iApp generator using existing configurations as a template"
   homepage "https://github.com/f5devcentral/f5-tmsh2iapp"
-  url "https://raw.githubusercontent.com/f5devcentral/f5-tmsh2iapp/00f44aa223b2aa3f9d394c0ef9171887dd24b22c/tmsh2iapp.pl"
+  url "https://github.com/f5devcentral/f5-tmsh2iapp.git", :revision => "00f44aa223b2aa3f9d394c0ef9171887dd24b22c"
   version "20180717.1"
-  sha256 "a8285de5f2f5055f42113a6d255af1797b81e8039737a2ee9164c047c3e9691f"
   head "https://github.com/f5devcentral/f5-tmsh2iapp.git"
 
   def install
