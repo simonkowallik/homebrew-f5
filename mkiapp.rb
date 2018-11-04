fb224dfd5fa822a6a0d4600adb41d3c2ab52be8e
Simon Kowallik <github@simonkowallik.com>
2018-11-04 13:46:09 +0100

    bump mkiapp to v1.1 - new sha256

--- mkiapp.rb
+++ mkiapp.rb
@@ -2,7 +2,7 @@ class Mkiapp < Formula
   desc "mkiapp simplifies the process of combining separate source files into an iApp - assisting the development lifecycle of your iApps"
   homepage "https://github.com/simonkowallik/mkiapp"
   url "https://github.com/simonkowallik/mkiapp/archive/v1.1.tar.gz"
-  sha256 "6efd28915f866ead4b65ecc626f0d8340c45107d4250fd4d852bfb1f843861ea"
+  sha256 "112df6390a22c188f83e889a3dafedd53563c082bca12cee79d99c905c1cc4af"
   head "https://github.com/simonkowallik/mkiapp.git"
 
   def install
