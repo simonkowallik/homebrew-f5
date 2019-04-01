e0e5da7d098c0bf6b9d6904118430ac4adbade94
Simon Kowallik <github@simonkowallik.com>
2019-04-01 20:17:23 +0200

    bump ihac.rb version to 3.0

--- ihac.rb
+++ ihac.rb
@@ -1,8 +1,8 @@
 class Ihac < Formula
   desc "iHealth API clients - simple shell scripts to interact with the F5 iHealth API (ihealth.f5.com)."
   homepage "https://github.com/simonkowallik/iHAC"
-  url "https://github.com/simonkowallik/iHAC/archive/2.2.tar.gz"
-  sha256 "4524219144fbc7779ca315577333dd90fada7e836733d3dc8fed9cde8b98cce4"
+  url "https://github.com/simonkowallik/iHAC/archive/3.0.tar.gz"
+  sha256 "debb456232d628c9ce390ec58c76f6a0452578ab00f099455b8a72c42e05a5b4"
   head "https://github.com/simonkowallik/iHAC.git"
 
   def install
