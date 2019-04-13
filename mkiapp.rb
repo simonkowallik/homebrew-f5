9586f611f4a659aace25e7ffaf52a4bd34ebaed0
Simon Kowallik <github@simonkowallik.com>
2019-04-13 10:37:08 +0200

    mkiapp.rb update to version 2.1

--- mkiapp.rb
+++ mkiapp.rb
@@ -1,8 +1,8 @@
 class Mkiapp < Formula
   desc "mkiapp simplifies the process of combining separate source files into an iApp - assisting the development lifecycle of your iApps"
   homepage "https://github.com/simonkowallik/mkiapp"
-  url "https://github.com/simonkowallik/mkiapp/archive/v2.0.tar.gz"
-  sha256 "6c55e1612a35fe45aa1236f3063cb144baead0c90e47d58b6fc73624b2db9119"
+  url "https://github.com/simonkowallik/mkiapp/archive/v2.1.tar.gz"
+  sha256 "6086fbc6c4c49d7ef5cfd2a8a64c048943f9a8aad2773a290b7f95f692e5d02b"
   head "https://github.com/simonkowallik/mkiapp.git"
 
   def install
