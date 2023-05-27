3629309c6572a207289d4f8975db3e5e565f7fbe
Simon Kowallik <sk-github@simonkowallik.com>
2023-05-27 18:08:34 +0200

    feat: ihac 4.0 update

--- ihac.rb
+++ ihac.rb
@@ -1,20 +1,21 @@
 class Ihac < Formula
   desc "iHealth API clients - simple shell scripts to interact with the F5 iHealth API (ihealth.f5.com)."
   homepage "https://github.com/simonkowallik/iHAC"
-  url "https://github.com/simonkowallik/iHAC/archive/3.1.tar.gz"
-  sha256 "a9a6b1772a21b8f0cbfc80931c647ea1c96ff82329eb52eab2bdc6939ab8a2cb"
+  url "https://github.com/simonkowallik/iHAC/archive/refs/tags/4.0.tar.gz"
+  sha256 "2c759a1321de875e6b3fae02ea73a2db33fcc61d7ec5f5ca48524b0386cbc878"
   head "https://github.com/simonkowallik/iHAC.git"
 
   def install
-    bin.install "ihac-auth"
-    bin.install "ihac-commandlist"
-    bin.install "ihac-commandrun"
-    bin.install "ihac-diagnostics"
-    bin.install "ihac-fileget"
-    bin.install "ihac-filelist"
-    bin.install "ihac-qkviewadd"
-    bin.install "ihac-qkviewdelete"
-    bin.install "ihac-qkviewget"
-    bin.install "ihac-qkviewlist"
+    bin.install "ihac"
+#    bin.install "ihac-commandlist"
+#    bin.install "ihac-commandrun"
+#    bin.install "ihac-diagnostics"
+#    bin.install "ihac-fileget"
+#    bin.install "ihac-filelist"
+#    bin.install "ihac-qkviewadd"
+#    bin.install "ihac-qkviewdelete"
+#    bin.install "ihac-qkviewmetadata"
+#    bin.install "ihac-qkviewget"
+#    bin.install "ihac-qkviewlist"
   end
 end
